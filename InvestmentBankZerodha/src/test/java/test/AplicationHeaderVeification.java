d5b2852ad6c809d4bfc8456516fe4973ad204191
Akshay G <Akshay G@AKSHAYG>
2022-02-16 15:56:33 +0530

    New Investment Bank Project

---
+++ InvestmentBankZerodha/src/test/java/test/AplicationHeaderVeification.java
@@ -0,0 +1,50 @@
+package test;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class AplicationHeaderVeification {
+	@BeforeTest
+	public void launchBrowser()
+	{
+		
+	}
+	@BeforeClass
+	public void createPOMObject()
+	{
+		
+	}
+	@BeforeMethod
+	public void loginZerodha()
+	{
+		
+	}
+	
+	@Test
+	public void verifyHoldingsTab(){
+		
+	}
+	
+	@AfterMethod
+	public void logOutZerodha()
+	{
+		
+	}
+	
+	@AfterClass
+	public void deletePOMObject() {
+		
+	}
+	
+	@AfterTest
+	public void closeBrowser() {
+		
+		
+	}
+
+}
---
+++ InvestmentBankZerodha/src/test/java/test/IntradayOrderExecution.java
@@ -0,0 +1,55 @@
+package test;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class IntradayOrderExecution {
+	@BeforeTest
+	public void launchBrowser()
+	{
+		
+	}
+	@BeforeClass
+	public void createPOMObject()
+	{
+		
+	}
+	@BeforeMethod
+	public void loginZerodha()
+	{
+		
+	}
+	
+	@Test
+	public void buyOrderOfTataMOtors(){
+		
+	}
+	
+	@Test
+	public void buyOrderOfDixonTech(){
+		
+	}
+	
+	@AfterMethod
+	public void logOutZerodha()
+	{
+		
+	}
+	
+	@AfterClass
+	public void deletePOMObject() {
+		
+	}
+	
+	@AfterTest
+	public void closeBrowser() {
+		
+		
+	}
+
+}
